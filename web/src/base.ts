b0d30f5105776393a62b3adf3702f15c11877a3d
kjaehr <kjaehr96@gmail.com>
2025-09-17 13:41:48 +0200

    News burde virke nu

--- web/src/base.ts
+++ web/src/base.ts
@@ -1,2 +1,3 @@
-export const BASE = process.env.NEXT_PUBLIC_BASE_PATH || ''
-export const DATA_BASE = process.env.NEXT_PUBLIC_DATA_BASE || 'https://raw.githubusercontent.com/Kjaehr/STOCK_DASHBOARD/main/data'
+const base = process.env.NEXT_PUBLIC_BASE_PATH || ''
+export const BASE = base
+export const DATA_BASE = process.env.NEXT_PUBLIC_DATA_BASE || `${base}/data`
